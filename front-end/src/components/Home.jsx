9a6d87b13aa4539865998d110b85fc6a92c883ca
Claire Rhodes <claire.rhodes13@gmail.com>
2024-12-11 15:01:37 -0800

    added navbar functionality to app.jsx, created ShowGame.jsx and GameIndex.jsx

---
+++ front-end/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <>
+            <h1> Welcome to Our App </h1>
+            <p> Slay pookie </p>
+        </>
+    );
+};
+
+export default Home;
--- front-end/src/components/games/GameIndex.jsx
+++ front-end/src/components/games/GameIndex.jsx
@@ -1,1 +1,19 @@
 // show all games
+
+import ShowGame from "./ShowGame";
+
+const GameIndex = ({games}) => {
+    return (
+        <div>
+            <ul>
+                {games.map((game) => {
+                    <li key={game.name}>
+                        <ShowGame game={game} />
+                    </li>
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default GameIndex;
