e962435e93a2ef0e43af10536b127e67c025e915
Claire Rhodes <claire.rhodes13@gmail.com>
2024-12-12 13:09:18 -0800

    coworking updates

--- front-end/src/components/games/GameIndex.jsx
+++ front-end/src/components/games/GameIndex.jsx
@@ -1,8 +1,24 @@
 // show all games
 
 import ShowGame from "./ShowGame.jsx";
+import { GAMES_URL } from "../../App.jsx";
+import { useState, useEffect } from 'react';
 
-const GameIndex = ({ games }) => {
+const GameIndex = () => {
+	const [games, setGames] = useState([]); // set state variable
+	useEffect(() => {
+		const getGamesIndex = async () => { // define function to call it
+			try {
+				const res = await fetch(GAMES_URL);
+				let JSONdata = await res.json();
+				setGames(JSONdata);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		getGamesIndex(); // do dis
+	}, []);
+
 	return (
 		<div>
 			<h2>Game Index</h2>
