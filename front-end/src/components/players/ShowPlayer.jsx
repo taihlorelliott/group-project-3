c5178beaaa3d02714d4fc835d7fb8d8fb5b4c4e9
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-12-16 17:04:46 -0500

    styling indexs/shows

--- front-end/src/components/players/ShowPlayer.jsx
+++ front-end/src/components/players/ShowPlayer.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { PLAYERS_URL } from "../../App";
+import '../../index.css';
 
+// bootstrap 
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const ShowPlayer = ({ handleSection, storedPlayerId }) => {
 	const [player, setPlayer] = useState({
@@ -23,30 +26,30 @@ const ShowPlayer = ({ handleSection, storedPlayerId }) => {
 	}, []);
 
 	return (
-		<>
-			<div className="ShowPlayerDiv">
-				<h3> {player.name} </h3>
-				<p> Favorite Game: {player.favoriteGame} </p>
-				<p> Games Played:</p>
+		<div className="bg-dark">
+			<div className="ms-2">
+				<h3 className="text-danger"> {player.name} </h3>
+				<p className="text-light"> Favorite Game: {player.favoriteGame} </p>
+				<p className="text-light"> Games Played:</p>
 				{player.gamesPlayed.length ? (
 					<ul>
 						{player.gamesPlayed.map((game, index) => (
-							<li key={index}>{game}</li>
+							<li className="text-light" key={index}>{game}</li>
 						))}
 					</ul>
 				) : (
-					<p>Try adding a game from our game section!</p>
+					<p className="text-light" >Try adding a game from our game section!</p>
 				)}
 			</div>
 			<div>
-				<button onClick={handleSection} value="EditPlayer">
+				<button className="btn btn-info me-2 btn-sm ms-2" onClick={handleSection} value="EditPlayer">
 					Edit
 				</button>
-				<button onClick={handleSection} value="PlayerIndex">
+				<button className="btn btn-info me-2 btn-sm ms-2" onClick={handleSection} value="PlayerIndex">
 					Back to Players
 				</button>
 			</div>
-		</>
+		</div>
 	);
 };
 
